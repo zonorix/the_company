6551b7d05b87089a66bcd197e1d789935cc461f8
Motozono <voodoo.baby1984@gmail,com>
2019-10-12 15:00:44 +0900

    menu-4price, contact-formレスポンシブ化。contact-formにattach-fileを追加。

--- js/script.js
+++ js/script.js
@@ -9,4 +9,21 @@ $(function(){
             $('.header-nav, .nav-bar').removeClass('active');
         }
     });
+
+    $('#imageFiles').css({
+        'position': 'absolute',
+        'top': '-9999px'
+    }).change(function () {
+        var val = $(this).val();
+        var path = val.replace(/\\/g, '/');
+        var match = path.lastIndexOf('/');
+        $('#fileName').css("display", "inline-block");
+        $('#fileName').val(match !== -1 ? val.substring(match + 1) : val);
+    });
+    $('#fileName').bind('keyup, keydown, keypress', function () {
+        return false;
+    });
+    $('#fileName, #imageFileBtn').click(function () {
+        $('#imageFiles').trigger('click');
+    });
 })
